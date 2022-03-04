0d0307f4b583d672b109b01e6e563300130dd497
wang-jie <421004034@qq.com>
2022-03-04 16:18:42 +0800

    ids4

--- NanoService.IdentityServer/Program.cs
+++ NanoService.IdentityServer/Program.cs
@@ -1,15 +1,26 @@
+﻿using System.Security.Claims;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.IdentityModel.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddIdentityServer(options => { IdentityModelEventSource.ShowPII = true; })
+    .AddInMemoryIdentityResources(Config.IdentityResources())
+    .AddInMemoryApiScopes(Config.ApiScopes())
+    .AddInMemoryApiResources(Config.ApiResources())
+    .AddInMemoryClients(Config.Clients())
+    .AddTestUsers(Config.Users())
+    //.AddResourceOwnerValidator<ResourceOwnerPasswordValidator>()
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -20,4 +31,110 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseIdentityServer();
+
 app.Run();
+
+static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources()
+    {
+        return new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+    }
+
+    public static IEnumerable<ApiScope> ApiScopes()
+    {
+        return new List<ApiScope>
+        {
+            new ApiScope(name: "customer.scope", displayName: "customer.scope"),
+            new ApiScope(name: "product.scope", displayName: "product.scope"),
+        };
+    }
+
+    public static IEnumerable<ApiResource> ApiResources()
+    {
+        return new List<ApiResource>
+        {
+            new ApiResource(name: "customer.resource", displayName: "customer.resource")
+            {
+                Scopes = new string[]
+                {
+                    "customer.scope"
+                }
+            },
+            new ApiResource(name: "product.resource", displayName: "product.resource")
+            {
+                Scopes = new string[]
+                {
+                    "product.scope"
+                }
+            },
+        };
+    }
+
+    public static List<TestUser> Users()
+    {
+        return new List<TestUser>
+        {
+            new TestUser
+            {
+                SubjectId = "1",
+                Username = "admin",
+                Password = "admin",
+                Claims = new[]
+                {
+                    new Claim("name", "admin"),
+                    new Claim("role", "admin"),
+                }
+            },
+            new TestUser
+            {
+                SubjectId = "2",
+                Username = "operator",
+                Password = "operator",
+                Claims = new[]
+                {
+                    new Claim("name", "operator"),
+                    new Claim("role", "operator"),
+                }
+            },
+            new TestUser
+            {
+                SubjectId = "3",
+                Username = "guest",
+                Password = "guest",
+                Claims = new[]
+                {
+                    new Claim("name", "guest"),
+                    new Claim("role", "guest"),
+                }
+            }
+        };
+    }
+
+    public static IEnumerable<Client> Clients()
+    {
+        return new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client_Credential",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = { "customer.scope", "product.scope" }
+            },
+
+            new Client
+            {
+                ClientId = "client_resourceOwnerPassword",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                AllowedScopes = { "customer.scope", "product.scope" }
+            }
+        };
+    }
+}
