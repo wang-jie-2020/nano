8f02c0fed156b5fcd7bf929fea699b01bc3f6dd4
wang-jie <421004034@qq.com>
2022-03-04 17:06:07 +0800

    加到服务里

---
+++ NanoService.IdentityServer/Config.cs
@@ -0,0 +1,104 @@
+﻿using System.Security.Claims;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace NanoService.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources()
+        {
+            return new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+        }
+
+        public static IEnumerable<ApiScope> ApiScopes()
+        {
+            return new List<ApiScope>
+        {
+            new ApiScope(name: "customer.scope", displayName: "customer.scope"),
+            new ApiScope(name: "product.scope", displayName: "product.scope"),
+        };
+        }
+
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new List<ApiResource>
+        {
+            new ApiResource(name: "admin.resource", displayName: "customer.resource")
+            {
+                Scopes = new string[]
+                {
+                    "customer.scope",
+                    "product.scope"
+                }
+            }
+        };
+        }
+
+        public static List<TestUser> Users()
+        {
+            return new List<TestUser>
+        {
+            new TestUser
+            {
+                SubjectId = "1",
+                Username = "admin",
+                Password = "admin",
+                Claims = new[]
+                {
+                    new Claim("name", "admin"),
+                    new Claim("role", "admin"),
+                }
+            },
+            new TestUser
+            {
+                SubjectId = "2",
+                Username = "operator",
+                Password = "operator",
+                Claims = new[]
+                {
+                    new Claim("name", "operator"),
+                    new Claim("role", "operator"),
+                }
+            },
+            new TestUser
+            {
+                SubjectId = "3",
+                Username = "guest",
+                Password = "guest",
+                Claims = new[]
+                {
+                    new Claim("name", "guest"),
+                    new Claim("role", "guest"),
+                }
+            }
+        };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client_Credential",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = { "customer.scope", "product.scope" }
+            },
+
+            new Client
+            {
+                ClientId = "client_resourceOwnerPassword",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                AllowedScopes = { "customer.scope", "product.scope" }
+            }
+        };
+        }
+    }
+}
--- NanoService.IdentityServer/Program.cs
+++ NanoService.IdentityServer/Program.cs
@@ -3,6 +3,8 @@ using IdentityServer4;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
 using Microsoft.IdentityModel.Logging;
+using NanoService.IdentityServer;
+using NanoService.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,108 +35,6 @@ app.MapControllers();
 
 app.UseIdentityServer();
 
+app.UseConsul(app.Configuration);
+
 app.Run();
-
-static class Config
-{
-    public static IEnumerable<IdentityResource> IdentityResources()
-    {
-        return new List<IdentityResource>
-        {
-            new IdentityResources.OpenId(),
-            new IdentityResources.Profile()
-        };
-    }
-
-    public static IEnumerable<ApiScope> ApiScopes()
-    {
-        return new List<ApiScope>
-        {
-            new ApiScope(name: "customer.scope", displayName: "customer.scope"),
-            new ApiScope(name: "product.scope", displayName: "product.scope"),
-        };
-    }
-
-    public static IEnumerable<ApiResource> ApiResources()
-    {
-        return new List<ApiResource>
-        {
-            new ApiResource(name: "customer.resource", displayName: "customer.resource")
-            {
-                Scopes = new string[]
-                {
-                    "customer.scope"
-                }
-            },
-            new ApiResource(name: "product.resource", displayName: "product.resource")
-            {
-                Scopes = new string[]
-                {
-                    "product.scope"
-                }
-            },
-        };
-    }
-
-    public static List<TestUser> Users()
-    {
-        return new List<TestUser>
-        {
-            new TestUser
-            {
-                SubjectId = "1",
-                Username = "admin",
-                Password = "admin",
-                Claims = new[]
-                {
-                    new Claim("name", "admin"),
-                    new Claim("role", "admin"),
-                }
-            },
-            new TestUser
-            {
-                SubjectId = "2",
-                Username = "operator",
-                Password = "operator",
-                Claims = new[]
-                {
-                    new Claim("name", "operator"),
-                    new Claim("role", "operator"),
-                }
-            },
-            new TestUser
-            {
-                SubjectId = "3",
-                Username = "guest",
-                Password = "guest",
-                Claims = new[]
-                {
-                    new Claim("name", "guest"),
-                    new Claim("role", "guest"),
-                }
-            }
-        };
-    }
-
-    public static IEnumerable<Client> Clients()
-    {
-        return new List<Client>
-        {
-            new Client
-            {
-                ClientId = "client_Credential",
-                ClientSecrets = {new Secret("secret".Sha256())},
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                AllowedScopes = { "customer.scope", "product.scope" }
-            },
-
-            new Client
-            {
-                ClientId = "client_resourceOwnerPassword",
-                ClientSecrets = {new Secret("secret".Sha256())},
-                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                AllowedScopes = { "customer.scope", "product.scope" }
-            }
-        };
-    }
-}
