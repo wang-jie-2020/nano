673574845c97b99f332d6d6636bd54d4391621f4
wang-jie <421004034@qq.com>
2022-03-07 15:50:45 +0800

    不使用动态路由 健康检查得写法,与其他项目写法不同

--- NanoService.Ocelot/Program.cs
+++ NanoService.Ocelot/Program.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using NanoService.Infrastructure.Extensions;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
@@ -23,26 +26,46 @@ builder.Host.UseSerilog();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
 
 builder.Configuration.AddJsonFile("./Configurations/ocelot.json");
 builder.Services.AddOcelot(builder.Configuration).AddConsul().AddPolly();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.RoutePrefix = "swagger";
-        c.SwaggerEndpoint("/CustomerService/swagger/v1/swagger.json", "Customer API");
-        c.SwaggerEndpoint("/ProductService/swagger/v1/swagger.json", "Product API");
-    });
-}
+    c.RoutePrefix = "swagger";
+    c.SwaggerEndpoint("/CustomerService/swagger/v1/swagger.json", "Customer API");
+    c.SwaggerEndpoint("/ProductService/swagger/v1/swagger.json", "Product API");
+});
 
+/*
+ *  在非路由项目中运行正常，但Ocelot中运行不正常
+ */
+//app.MapHealthChecks("/healthz");
+/*
+ *  在非路由项目中运行正常，但Ocelot中运行不正常
+ */
+//app.MapGet("/healthz", async handler =>
+//{
+//    await handler.Response.WriteAsync("healthy");
+//});
+
+app.Map("/healthz", b =>
+{
+    b.Run(async handler =>
+    {
+        await handler.Response.WriteAsync("ok");
+    });
+});
+
+app.MapControllers();
 app.UseAuthentication();
 app.UseAuthorization();
-app.MapControllers();
+
+app.UseConsul(app.Configuration);
 app.UseOcelot().Wait();
 
 app.Run();
