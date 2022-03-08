5d503610ea5c414fb4f2fec7fbb418594626c579
wang-jie <421004034@qq.com>
2022-03-08 14:12:37 +0800

    简单的模拟了一下实际的user claim输出

--- NanoService.IdentityServer/Extend/ExtendClaimService.cs
+++ NanoService.IdentityServer/Extend/ExtendClaimService.cs
@@ -1,16 +1,29 @@
 ﻿using System.Security.Claims;
+using IdentityModel;
+using IdentityServer4;
 using IdentityServer4.AspNetIdentity;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
 using IdentityServer4.Services;
 using IdentityServer4.Test;
 using IdentityServer4.Validation;
 
 namespace NanoService.IdentityServer.Extend
 {
+    /*
+     *  IClaimService 是为identity token 和 access token 建立claims集合的，同时调用IProfileService来赋值
+     */
     public class ExtendClaimService : DefaultClaimsService, IClaimsService
     {
         public ExtendClaimService(IProfileService profile, ILogger<DefaultClaimsService> logger)
             : base(profile, logger)
         {
+        }
+
+        protected override IEnumerable<string> FilterRequestedClaimTypes(IEnumerable<string> claimTypes)
+        {
+            var claims = new[] { "name" };
+            return base.FilterRequestedClaimTypes(claimTypes).Union(claims);
         }
     }
 }
--- NanoService.IdentityServer/Extend/ExtendProfileService.cs
+++ NanoService.IdentityServer/Extend/ExtendProfileService.cs
@@ -1,4 +1,6 @@
-﻿using IdentityServer4.AspNetIdentity;
+﻿using System.Security.Claims;
+using IdentityServer4.AspNetIdentity;
+using IdentityServer4.Extensions;
 using IdentityServer4.Models;
 using IdentityServer4.Services;
 using IdentityServer4.Test;
@@ -6,6 +8,10 @@ using Microsoft.AspNetCore.Identity;
 
 namespace NanoService.IdentityServer.Extend
 {
+    /*
+     *  IProfileService 是为IClaimService中的claims集合赋值的
+     *  在Default上简单实现一下，ProfileService<TUser>是基于Identity实现
+     */
     public class ExtendProfileService : DefaultProfileService, IProfileService
     {
         public ExtendProfileService(ILogger<DefaultProfileService> logger) : base(logger)
@@ -19,13 +25,22 @@ namespace NanoService.IdentityServer.Extend
             context.AddRequestedClaims(context.Subject.Claims);
             context.LogIssuedClaims(this.Logger);
             return Task.CompletedTask;
-        }
 
-        public override Task IsActiveAsync(IsActiveContext context)
-        {
-            //from base
-            context.IsActive = true;
-            return Task.CompletedTask;
+            ////copy&simplify from AspNetCore.Identity
+            //ClaimsPrincipal subject = context.Subject;
+            //string? subjectId = subject?.GetSubjectId();
+            //if (subjectId == null)
+            //    throw new Exception("No sub claim present");
+
+            //TestUser userAsync = Config.Users().FirstOrDefault(p => p.SubjectId == subjectId);
+            //if ((object)userAsync == null)
+            //    return Task.CompletedTask;
+
+            ////real: await this.ClaimsFactory.CreateAsync(user) ?? throw new Exception("ClaimsFactory failed to create a principal");
+            //var claims = userAsync.Claims;
+
+            ////same name claim will be filled
+            //context.AddRequestedClaims(claims);
         }
     }
 }
